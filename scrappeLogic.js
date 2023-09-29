61c765bcc1b65bb5987dc1a47d6cbeddbfdd74d6
Marcos <magma_ruby145@hotmail.com>
2023-09-29 14:53:47 -0700

    Deploy test 2.2 se pusieron los args y un path ejecutable lo que sea que signifique eso :D

--- scrappeLogic.js
+++ scrappeLogic.js
@@ -1,9 +1,21 @@
 const puppeteer = require("puppeteer");
+require("dotenv").config();
 const url = "https://enlinea2023-2.uabcs.mx/login/";
 const desktop = "https://enlinea2023-2.uabcs.mx/my/";
 const scrapeLogic = async (res) => {
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args:[
+            "--disable-setuid-sandbox",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote"
+        ],
+        executablePath: 
+        process.env.NODE_ENV === "production"
+            ? process.env.PUPPETEER_EXCECUTABLE_PATH
+            : puppeteer.executablePath(),
+    });
     try{
     const page = await browser.newPage();
     await page.goto(url);
